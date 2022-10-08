4cae240aca888ca9b7c7dc89a9ada6ab325b1761
Johan Andersson <johan-andersson01@protonmail.com>
2022-10-08 15:45:33 +0200

    Support multiple images

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use ansi_term::{Style, Colour::RGB};
-use image::{DynamicImage, Pixel, ImageError};
+use ansi_term::{Colour::RGB, Style};
+use image::{DynamicImage, ImageError, Pixel};
 
 fn usage(bin_name: &String) {
-    println!("usage: {} <PATH>", bin_name);
+    println!("usage: {} IMAGE_PATH [IMAGE_PATH ...]", bin_name);
     std::process::exit(1);
 }
 
@@ -16,36 +16,12 @@ fn print_color(text: &String, fg: (u8, u8, u8), bg: (u8, u8, u8)) {
     );
 }
 
-fn preprocess_image(
-    path: &String,
-    width: u16,
-    height: u16,
-) -> Result<DynamicImage, ImageError> {
+fn preprocess_image(path: &String, width: u16, height: u16) -> Result<DynamicImage, ImageError> {
     let img = image::open(path)?;
     Ok(img.thumbnail(width.into(), height.into()))
 }
 
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() < 2 {
-        usage(&args[0]);
-    }
-    let path = &args[1];
-
-    let (term_w, term_h) = match termion::terminal_size() {
-        Ok(result) => result,
-        Err(error) => {
-            println!("Failed to get terminal size: {}", error);
-            std::process::exit(1);
-        }
-    };
-
-    // Image should be viewable within current window, and allow for one extra row for the command
-    // prompt. Multiply height with two, since we represent 2 pixels per character (foreground /
-    // background).
-    let w = term_w;
-    let h = term_h * 2 - 1;
-
+fn draw_img(path: &String, w: u16, h: u16) {
     let img = match preprocess_image(path, w, h) {
         Ok(result) => result,
         Err(error) => {
@@ -81,4 +57,30 @@ fn main() {
         println!("");
     }
     // TODO: if #rows is odd, we currently skip the last row
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        usage(&args[0]);
+    }
+    let paths = &args[1..];
+
+    let (term_w, term_h) = match termion::terminal_size() {
+        Ok(result) => result,
+        Err(error) => {
+            println!("Failed to get terminal size: {}", error);
+            std::process::exit(1);
+        }
+    };
+
+    // Image should be viewable within current window, and allow for one extra row for the command
+    // prompt. Multiply height with two, since we represent 2 pixels per character (foreground /
+    // background).
+    let w = term_w;
+    let h = term_h * 2 - 1;
+
+    for path in paths {
+        draw_img(path, w, h);
+    }
 }
